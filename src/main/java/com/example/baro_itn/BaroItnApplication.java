b902dd97c4d196c789663227b9965e93f0a3c584
Woojin1123 <boom175839@gmail.com>
2025-05-15 00:27:31 +0900

    init commit

---
+++ src/main/java/com/example/baro_itn/BaroItnApplication.java
@@ -0,0 +1,13 @@
+package com.example.baro_itn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaroItnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaroItnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/baro_itn/BaroItnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baro_itn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaroItnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
