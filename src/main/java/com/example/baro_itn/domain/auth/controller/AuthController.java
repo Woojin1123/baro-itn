a223116bd8e5304ffa86023885948f096e122d1d
Woojin1123 <boom175839@gmail.com>
2025-05-15 21:12:04 +0900

    [Fix] swagger 수정

--- src/main/java/com/example/baro_itn/domain/auth/controller/AuthController.java
+++ src/main/java/com/example/baro_itn/domain/auth/controller/AuthController.java
@@ -33,8 +33,8 @@ public class AuthController {
                     schema = @Schema(implementation = LoginRequestDto.class),
                     examples = @ExampleObject(value = """
                             {
-                              "username": "김우진",
-                              "password": "password"
+                              "username": "admin",
+                              "password": "admin"
                             }
                             """)
             )
